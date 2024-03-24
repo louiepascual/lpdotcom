bb454b9e3ee203645ed8cf0f8ab25f053402c798
Louie Pascual <louieepascual@gmail.com>
2024-03-24 23:22:34 +0800

    return a 404 on non-root requests in home handler
    Signed-off-by: Louie Pascual <louieepascual@gmail.com>

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
+
     w.Write([]byte("hello world! lpdotcom staging"))
+
 }
 
 func main() {
